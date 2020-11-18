cfa3c5fec9dc9c143fb52b3e6aaf488e247e8130
unknown <wil.macedo.sa@gmail.com>
2020-11-18 01:13:28 -0300

    Condição de porta do server adicionada

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const app = require('./bin/express');
 const zeusRouter = require('./module/zeus/routes');
 const bodyParser = require('body-parser');
 
-const port = 3333;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use('/api/zeus', zeusRouter);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`API init in port ${port}`);
 });
